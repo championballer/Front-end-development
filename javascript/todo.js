8d4608abffed1f36b402de57f9f49005221d17b2
Shiv Kumar <championballer08@gmail.com>
2018-09-04 18:43:11 +0530

    added todolist with hover event and stikethrough, using loops

---
+++ javascript/todo.js
@@ -0,0 +1,20 @@
+var lis = document.querySelectorAll("li");
+var clicked = [false,false,false];
+
+alert(clicked.length);
+for(var i = 0;i<lis.length;i++)
+{
+	lis[i].addEventListener("mouseover",function(){
+		this.classList.add("colorchange");
+	});
+	
+	lis[i].addEventListener("mouseout",function(){
+		this.classList.remove("colorchange");
+	});
+	
+	lis[i].addEventListener("click",function(){
+		this.classList.add("strike");
+		this.classList.add("colorchange");	
+			
+	});
+}
