ee835548c8a0b98d08326e409eb87af43eb8b166
Shiv Kumar <championballer08@gmail.com>
2018-09-04 12:38:31 +0530

    added source for score keeper

---
+++ javascript/scorekeeper.js
@@ -0,0 +1,9 @@
+var p1score = document.querySelector("#p1score");
+var p2score = document.querySelector("#p2score");
+
+
+alert(p1score.textContent);
+alert(p2score.textContent);
+
+p1score.textContent = 1;
+//p1score.innerHTML = "<span>1</span> to <span>5</span>";
