b5138cac9eb1c80b00813fb50747a0bd59bfe79d
Shiv Kumar <championballer08@gmail.com>
2018-09-05 17:48:55 +0530

    added changed to javascript to respond to click

--- colorgame/colorgame.js
+++ colorgame/colorgame.js
@@ -6,8 +6,27 @@ var colors = ["rgb(255, 0, 0)",
 		"rgb(255, 0, 255)"];
 	 
 var squares = document.querySelectorAll(".square");
+var colorSelected = colors[3];
 
+var helement = document.querySelector("#helement");
+helement.textContent = colorSelected;
 for(var i = 0;i<squares.length;i++)
 {
-	squares[i].style.background = colors[i];
+	squares[i].style.backgroundColor = colors[i];
+	
+	squares[i].addEventListener("click",function(){
+		var clicked = this.style.backgroundColor;
+		if(clicked == colorSelected)
+		{
+			alert("Correct");
+		}
+		
+		else
+		{
+			alert("Wrong");
+		}
+		
+	});
 }	  
+
+
