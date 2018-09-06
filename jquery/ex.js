ef3c45cadfb6b0df66fcac00e3ac6ab3c4818d7c
Shiv Kumar <championballer08@gmail.com>
2018-09-07 00:52:01 +0530

    jquery init

---
+++ jquery/ex.js
@@ -0,0 +1,10 @@
+var objects = {
+	"background":"purple",
+	"margin":"10px"
+};
+
+
+$("div").css(objects);
+$(".highlight").css("width","200px");
+$("#third").css("border","5px solid orange");
+$("div").first().css("color","pink")
