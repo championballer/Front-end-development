b8309ac5f68108d6943364fd50965adc5c15a485
Shiv Kumar <championballer08@gmail.com>
2018-09-07 18:59:33 +0530

    js structure added

--- todo/assets/js/todo.js
+++ todo/assets/js/todo.js
@@ -1,11 +1,20 @@
-$("li").on("click",function(){
+$("ul").on("click","li",function(){
 	$(this).toggleClass("strike");
 });
 
-$("span").on("click",function(event){
+$("ul").on("click","span",function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which===13)
+	{
+		var content = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X </span>"+content+"</li>");
+	}
 });
